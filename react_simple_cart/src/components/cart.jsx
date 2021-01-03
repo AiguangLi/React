fff0c3b33cc717fe8656c2212966df5d75461e4c
李爱光 <aiguang.li@qq.com>
2021-01-03 17:27:38 +0800

    feature：将counter修改为受控组件

--- react_simple_cart/src/components/cart.jsx
+++ react_simple_cart/src/components/cart.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { getGoods } from '@/services/goods';
 import Tables from '@/components/tables';
 import Counter from '@/components/counter';
+import { throwStatement } from 'babel-types';
 
 export default class Cart extends Component {
 	constructor() {
@@ -29,26 +30,62 @@ export default class Cart extends Component {
 				{this.state.counters.map(counter => (
 					<Counter
 						key={counter.id}
-						{...counter}
+						counter={counter}
 						handleDelete={this.handleDeleteCounter}
+						handleIncrease={this.handleIncreaseCounter}
+						handleDecrease={this.handleDecreaseCounter}
 					></Counter>
 				))}
+				<button onClick={() => this.resetCounters()} className="btn btn-primary btn-sm m-2">重置</button>
 				<Tables goods={this.state.goods} handleDelete={this.handleDeleteGoods}></Tables>
 			</div>
 		);
 	}
 
 	handleDeleteGoods = goodsId => {
-		let newGoods = this.state.goods.filter(item => item.id !== goodsId);
+		const newGoods = this.state.goods.filter(item => item.id !== goodsId);
 		this.setState({
 			goods: newGoods,
 		});
 	};
 
-	handleDeleteCounter = counterId => {
-		let newCounters = this.state.counters.filter(item => item.id !== counterId);
+	resetCounters = () => {
+		const newCounters = this.state.counters.map(item => {
+			item.value = 0;
+
+			return item;
+		});
+
 		this.setState({
 			counters: newCounters,
+		});
+	}
+
+	handleDeleteCounter = counterId => {
+		const newCounters = this.state.counters.filter(item => item.id !== counterId);
+		this.setState({
+			counters: newCounters,
+		});
+	};
+
+	handleIncreaseCounter = counter => {
+		const counters = [...this.state.counters];
+		const index = counters.indexOf(counter);
+		counters[index].value ++;
+
+		this.setState({
+			counters,
+		});
+	};
+
+	handleDecreaseCounter = counter => {
+		
+		const counters = [...this.state.counters];
+		const index = counters.indexOf(counter);
+		counters[index].value  = counters[index].value - 1 > 0 ? counters[index].value - 1 : 0;
+
+		this.setState({
+			counters,
 		});
 	};
 
--- react_simple_cart/src/components/counter.jsx
+++ react_simple_cart/src/components/counter.jsx
@@ -1,36 +1,27 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-	constructor() {
-		super();
-		//console.log(this.props);
-		this.state = { id: null, value: 0 };
-	}
 
-	componentDidMount() {
-		this.setState({
-			id: this.props.id,
-			value: this.props.value,
-		});
+	constructor(props) {
+		super(props);
 	}
 
 	render() {
-		console.log('Hello');
 		return (
 			<div>
 				<button
 					className="btn btn-warning btn-sm m-1"
 					onClick={() => {
-						this.decreaseCount();
+						this.props.handleDecrease(this.props.counter);
 					}}
 				>
 					-
 				</button>
-				<span className={this.getGoodsCountStyles()}>{this.state.value}</span>
+				<span className={this.getGoodsCountStyles()}>{this.props.counter.value}</span>
 				<button
 					className="btn btn-primary btn-sm m-1"
 					onClick={() => {
-						this.increaseCount();
+						this.props.handleIncrease(this.props.counter);
 					}}
 				>
 					+
@@ -39,7 +30,7 @@ class Counter extends Component {
 				<button
 					className="btn btn-danger btn-sm m-1"
 					onClick={() => {
-						this.props.handleDelete(this.state.id);
+						this.props.handleDelete(this.props.counter.id);
 					}}
 				>
 					删除
@@ -48,23 +39,9 @@ class Counter extends Component {
 		);
 	}
 
-	increaseCount = () => {
-		this.setState({
-			value: this.state.value + 1,
-		});
-	};
-
-	decreaseCount = () => {
-		const newValue = this.state.value - 1;
-		this.setState({
-			value: newValue >= 0 ? newValue : 0,
-		});
-	};
-
 	getGoodsCountStyles = () => {
 		const styles = 'badge m-2 badge-';
-
-		return this.state.value === 0 ? styles + 'warning' : styles + 'primary';
+		return this.props.counter.value === 0 ? styles + 'warning' : styles + 'primary';
 	};
 }
 
