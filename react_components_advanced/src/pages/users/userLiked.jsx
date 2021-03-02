a886a524d6fd201e0b2243d41ac307ce7c436cc0
李爱光 <aiguang.li@qq.com>
2021-03-02 19:01:19 +0800

    feature：增加勾子函数示例

--- react_components_advanced/src/pages/users/userLiked.jsx
+++ react_components_advanced/src/pages/users/userLiked.jsx
@@ -1,14 +1,31 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
-class UserLiked extends Component {
-	state = {};
-	render() {
-		return (
-			<div>
-				<h4>User Liked</h4>
-			</div>
-		);
-	}
-}
+const UserLiked = () => {
+	const [count, setCount] = useState(0);
+	const [loading, setLoading] = useState(true);
+
+	const handleButtonClick = () => {
+		setCount(count + 1);
+	};
+
+	useEffect(() => {
+		setTimeout(() => {
+			setLoading(false);
+		}, 1000);
+
+		return () => {
+			console.log('Clean up!');
+		};
+	});
+
+	return (
+		<div>
+			<h4>User Liked</h4>
+			<button className="btn btn-primary small" onClick={handleButtonClick}>
+				{loading ? 'Loading...' : `Clicked (${count})`}
+			</button>
+		</div>
+	);
+};
 
 export default UserLiked;
